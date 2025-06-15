934b8c588f350519040ccb649c59ab10725102b7
Andrea Rossi <andrea.rossi0507@gmail.com>
2025-06-16 04:42:18 +0700

    [CHORE] Set up Inertia.js with React

---
+++ resources/js/Pages/Test.jsx
@@ -0,0 +1,16 @@
+import { Head } from "@inertiajs/react";
+
+const Test = () => {
+    return (
+        <>
+            <Head title="Test Page" />
+            <div>
+                <h1>Test Page</h1>
+                <p>This is a test page to verify the setup.</p>
+            </div>
+        </>
+
+    );
+}
+
+export default Test;
